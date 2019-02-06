5222cf725ff286e343cffb94db46591d41f3beef
sballari <ballarinsimone@gmail.com>
2019-02-06 22:45:57 +0100

    introduced simple sub typ alg

--- SimpleCalc-TC/src/SCStructures.hs
+++ SimpleCalc-TC/src/SCStructures.hs
@@ -13,5 +13,16 @@ module SCStructures where
         deriving (Show, Eq)
 
     data Type =
-        TBool | TNat | TArrow Type Type |TVar String
+        TBool | TNat | TArrow Type Type | Top | TVar String
         deriving (Show, Eq)
+
+    instance Ord Type where
+        --subtyping checker
+        _ <= Top = True
+        TNat <= TBool = True
+        TArrow s1 s2  <= TArrow t1 t2 = (t1 <= s1) && (s2 <=t2)
+        _ <= _ = False
+
+        s < t = s<=t && s/=t
+        s > t = t < s 
+        s >= t = t <= s
