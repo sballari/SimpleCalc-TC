81a690c0f187551a5a1aef0da543028beda07058
sballari <ballarinsimone@gmail.com>
2019-02-10 20:52:07 +0100

    added test to the suit

--- SimpleCalc-TC/test/Spec.hs
+++ SimpleCalc-TC/test/Spec.hs
@@ -3,10 +3,11 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 import ParserTest
+import InferenceTest
 
 main :: IO ()
 main = do
   defaultMain (testGroup "Library tests" tests)
     where
-      tests = ParserTest.tests
+      tests = ParserTest.tests ++ InferenceTest.tests
 
