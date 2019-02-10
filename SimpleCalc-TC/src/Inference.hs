3726c7ff3e0875f427b2d25b1a686709dddec947
sballari <ballarinsimone@gmail.com>
2019-02-10 23:20:24 +0100

    error managment improved

--- SimpleCalc-TC/src/Inference.hs
+++ SimpleCalc-TC/src/Inference.hs
@@ -6,11 +6,14 @@ module Inference where
 
     typeOfExe :: Term -> Maybe Type
     typeOfExe t = fst (app (typeOf [] t) [])
-    
+
+    enrichError :: String -> ST (Maybe Type)
+    enrichError err = ST(\e-> (Nothing,e++[err]))
+
     typeOf :: Contest -> Term -> ST (Maybe Type)
     typeOf ctx (EVar x) =
             let mtx = lookup x ctx in      
-            if mtx == Nothing then enrichError ("var "++x++" not in the context") >>= (\x -> return Nothing)
+            if mtx == Nothing then enrichError ("var "++x++" not in the context") 
             else return mtx    
             
     typeOf ctx (Efn x s term) = do  mt <- typeOf ((x,s):ctx) term 
@@ -27,20 +30,20 @@ module Inference where
                                             Nothing -> return Nothing
                                             Just t2 -> if t2 <= t11     
                                                         then return (Just t12)
-                                                        else enrichError ("parametrer type mismatch") >>= \x -> return Nothing 
-                _ -> enrichError("arrow type expected") >>= \x-> return Nothing
+                                                        else enrichError ("parametrer type mismatch")  
+                _ -> enrichError("arrow type expected")
     
     typeOf ctx (ECond bexp m1 m2) = 
         do 
             mt1 <- typeOf ctx m1
             mt2 <- typeOf ctx m2
             case mt1 of 
-                Nothing -> enrichError ("first branch typing error") >>= \x -> return Nothing
+                Nothing -> enrichError ("first branch typing error") 
                 Just t1 -> case mt2 of 
-                                Nothing -> enrichError ("second branch typing error") >>= \x -> return Nothing
+                                Nothing -> enrichError ("second branch typing error") 
                                 Just t2 -> let tif = lub t1 t2 in
                                             if tif == Nothing
-                                                then enrichError ("impossible joint of if stm") >>= \x -> return Nothing
+                                                then enrichError ("impossible joint of if stm") 
                                                 else return tif
 
 
@@ -49,9 +52,18 @@ module Inference where
             mt1 <- typeOf ctx m1
             mt2 <- typeOf ctx m2 
             case pure(\x y -> x <= TNat && y <=TNat) <*> mt1 <*> mt2 of 
-                Nothing -> enrichError ("impossible to type a operator of the sum") >>= \x -> return Nothing   
+                Nothing -> enrichError ("impossible to type a operator of the sum")   
                 Just True -> return (Just TNat)
-                Just False -> enrichError ("one of the operator is not a subtype of TNat") >>= \x -> return Nothing   
+                Just False -> enrichError ("one of the operator is not a subtype of TNat")
+                
+    typeOf ctx (EMin m1 m2) = 
+        do 
+            mt1 <- typeOf ctx m1
+            mt2 <- typeOf ctx m2 
+            case pure(\x y -> x <= TNat && y <=TNat) <*> mt1 <*> mt2 of 
+                Nothing -> enrichError ("impossible to type a operator of the sum")   
+                Just True -> return (Just TNat)
+                Just False -> enrichError ("one of the operator is not a subtype of TNat")
 
     typeOf ctx (EBool b) = return (Just TBool)
     typeOf ctx (ENum n) = return (Just TNat)
